ede7bddfd4ccd18d202d36b0b3790a0300489901
TrinHalfhand <mason.malia.janet@gmail.com>
2019-06-03 19:09:01 -0700

    Working on pure CSS page in CheckGameCore

---
+++ CheckerGameCore/Pages/CheckerBoard.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CheckerGameCore.Pages
+{
+    public class CheckerBoardModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
